505129fa2be86e872d8a2c6dcc08a9ff63984c7c
Vincent <vracelle@adneom.lu>
2020-09-24 14:17:59 +0200

    Updates

---
+++ precache-manifest.36ee0d18f7238daa8fe143408608c4fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a903456c7369d5a9bf61af240fa4e97",
+    "url": "./index.html"
+  },
+  {
+    "revision": "626f3702aac890d33be6",
+    "url": "./static/css/main.7502aeaf.chunk.css"
+  },
+  {
+    "revision": "e19e26a80efde07856c9",
+    "url": "./static/js/2.66a55965.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.66a55965.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "626f3702aac890d33be6",
+    "url": "./static/js/main.f287b084.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.f287b084.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18cda0e82ee8a03e3d19",
+    "url": "./static/js/runtime-main.f8a1d4ba.js"
+  }
+]);
