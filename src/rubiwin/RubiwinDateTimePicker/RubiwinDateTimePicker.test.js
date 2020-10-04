ccdac55897501cc0edab6254e2a693fb06e8e594
Vincent <vracelle@adneom.lu>
2020-10-04 16:21:19 +0200

    add datetime picker

---
+++ src/rubiwin/RubiwinDateTimePicker/RubiwinDateTimePicker.test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import RubiwinDateTimePicker from './RubiwinDateTimePicker'
+
+const testProps = {
+  label: 'test',
+  value: new Date('2018-01-01T00:00:00.000Z'),
+  onChange: () => {},
+  emptyLabel: 'select a date',
+  className: 'testClass'
+}
+
+it('renders correctly props object', () => {
+  const tree = renderer
+    .create(<RubiwinDateTimePicker {...testProps} />)
+    .toJSON()
+  expect(tree).toMatchSnapshot()
+})
