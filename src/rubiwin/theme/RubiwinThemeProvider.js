38f2e0ea7f06dacfa641fdf0ffbccc60fc3f1402
Vincent Racelle <vracelle@positivethinking.lu>
2021-08-13 12:05:13 +0200

    fix constants

--- src/rubiwin/theme/RubiwinThemeProvider.js
+++ src/rubiwin/theme/RubiwinThemeProvider.js
@@ -32,7 +32,7 @@ const theme = createMuiTheme({
       color: themeConstants.grey.main,
       fontSize: '32px',
       lineHeight: '39px',
-      [`@media only screen and (max-width: ${600}px)`]: {
+      [`@media only screen and (max-width: ${themeConstants.breakpoints.sm}px)`]: {
         fontSize: '22px',
         lineHeight: '26px'
       }
