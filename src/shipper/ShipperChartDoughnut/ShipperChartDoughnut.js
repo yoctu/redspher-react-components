2749de2250d7ceed3923c1120171d42a8a87f05f
TheSmartHyena <skopal.philippe@gmail.com>
2021-09-17 17:28:14 +0200

    Better chart doughnut

--- src/shipper/ShipperChartDoughnut/ShipperChartDoughnut.js
+++ src/shipper/ShipperChartDoughnut/ShipperChartDoughnut.js
@@ -2,8 +2,27 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Doughnut } from 'react-chartjs-2'
 
-const ShipperChartDoughnut = ({ data }) => {
-  return <Doughnut data={data} />
+const ShipperChartDoughnut = ({ data, ...delegated }) => {
+  const config = {
+    cutout: 90,
+    elements: {
+      arc: {
+        borderWidth: 0
+      }
+    },
+    plugins: {
+      legend: {
+        display: true,
+        labels: {
+          color: 'rgb(255, 99, 132)',
+          boxWidth: 12
+        },
+        position: 'left'
+      }
+    }
+  }
+
+  return <Doughnut data={data} {...delegated} options={config} />
 }
 
 ShipperChartDoughnut.propTypes = {
