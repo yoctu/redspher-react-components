c15404644287f795439fad1c68a1d46ea0cb710b
TheSmartHyena <skopal.philippe@gmail.com>
2021-09-25 16:51:12 +0200

    advancing

--- src/shipper/ShipperChartMapFlow/ShipperChartMapFlow.js
+++ src/shipper/ShipperChartMapFlow/ShipperChartMapFlow.js
@@ -1,27 +1,56 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
-import { Card, CardContent, CardHeader } from '@material-ui/core'
-import style from './ShipperChartMap.module.scss'
 import * as d3 from 'd3'
+import { geoMercator, geoPath } from 'd3-geo'
+import { Card, CardContent, CardHeader } from '@material-ui/core'
+import * as topojson from 'topojson-client'
+import countries from './countries.json'
 
-const ShipperChartMap = ({ data, title }) => {
+const ShipperChartMapFlow = ({ data, title }) => {
+  const width = 500
+  const height = 500
+  const svgRef = useRef()
 
+  useEffect(() => {
+    const projection = geoMercator()
+      .scale(150)
+      .translate([width / 2, height / 2])
+
+    const path = geoPath().pointRadius(2).projection(projection)
+    const svgEl = d3.select(svgRef.current)
+    svgEl.selectAll('*').remove()
+
+    // const airports = await d3.json('./airports.topo.json')
+    console.log(countries)
+
+    /*  svgEl
+        .append('g')
+        .attr('class', 'countries')
+        .selectAll('path')
+        .data(
+            topojson.feature(result.countries, result.countries.objects.countries)
+            .features
+        )
+        .enter()
+        .append('path')
+        .attr('d', path) */
+  })
 
   return (
     <Card>
-      <CardHeader title={title} className={style.centerTitle} />
+      <CardHeader title={title} />
       <CardContent>
-
+          
       </CardContent>
     </Card>
   )
 }
 
-ShipperChartMap.propTypes = {
+ShipperChartMapFlow.propTypes = {
   /** data to be displayed in the map chart */
   data: PropTypes.object.isRequired,
   /** Text to be displayed as title of the Chart Map card */
   title: PropTypes.string.isRequired
 }
 
-export default ShipperChartMap
+export default ShipperChartMapFlow
