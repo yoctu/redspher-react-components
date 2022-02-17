42bff9c56a1481a64fb01ac5cf95e19eade5dbca
Vincent Racelle <vincentracelle@MacBook-Pro-de-Vince.local>
2022-02-17 14:31:04 +0100

    test theme

--- src/rubiwin/theme/RubiwinBaseTheme.js
+++ src/rubiwin/theme/RubiwinBaseTheme.js
@@ -1,8 +1,6 @@
 import themeConstants from './themeConstants'
-import { darken } from '@mui/material/styles'
 
 const rubiwinBaseTheme = {
-  spacing: 4,
   palette: {
     primary: {
       main: themeConstants.primary.main,
@@ -71,6 +69,116 @@ const rubiwinBaseTheme = {
         }
     }
   },
+  overrides: {
+    MuiButton: {
+      contained: {
+        boxShadow: 'none',
+        '&:hover': {
+          boxShadow: 'none'
+        }
+      },
+      root: {
+        borderRadius: '8px',
+        fontSize: '14px',
+        textTransform: 'none'
+      },
+      containedPrimary: {
+        color: themeConstants.white.main
+      },
+      iconSizeLarge: {
+        '&> *:first-child': {
+          fontSize: '25px'
+        }
+      }
+    },
+    MuiListItem: {
+      root: {
+        '&$selected': {
+          backgroundColor: themeConstants.primary.main,
+          color: themeConstants.white.main,
+          '&:hover': {
+            backgroundColor: themeConstants.primary.main + 'FF'
+          }
+        }
+      }
+    },
+    MuiTableCell: {
+      sizeSmall: {
+        padding: '6px 12px'
+      },
+      stickyHeader: {
+        backgroundColor: themeConstants.grey.main
+      },
+      root: {
+        '&:first-of-type': {
+          borderLeft: '1px solid transparent'
+        },
+        '&:last-of-type': {
+          borderRight: '1px solid transparent'
+        },
+        borderRight: '1px solid ' + themeConstants.grey.lighter,
+        borderLeft: '1px solid ' + themeConstants.grey.lighter,
+        borderBottom: '1px solid ' + themeConstants.grey.lighter
+      },
+      head: {
+        backgroundColor: themeConstants.grey.main,
+        color: 'white',
+        borderTop: '0px !important'
+      },
+      body: {
+        color: themeConstants.grey.main + '!important',
+        fontSize: '12px !important'
+      }
+    },
+    MuiTableRow: {
+      root: {
+        '&:nth-of-type(even)': {
+          backgroundColor: themeConstants.grey.lighter
+        }
+      }
+    },
+    MuiCheckbox: {
+      root: {
+        color: themeConstants.grey.light
+      }
+    },
+    MuiDivider: {
+      root: {
+        marginTop: '10px',
+        marginBottom: '10px',
+        backgroundColor: themeConstants.grey.lighter
+      }
+    },
+    MuiTableSortLabel: {
+      root: { color: themeConstants.grey.lighter + '!important', opacity: 1 },
+      active: { color: themeConstants.grey.lighter, opacity: 1 },
+      icon: { color: themeConstants.grey.lighter, opacity: 1 }
+    },
+    MuiFormLabel: {
+      root: {
+        color: themeConstants.grey.main,
+        textTransform: 'capitalize'
+      }
+    },
+    MuiChip: {
+      colorPrimary: {
+        color: 'white'
+      }
+    },
+    MuiSwitch: {
+      thumb: {
+        backgroundColor: themeConstants.primary.main
+      }
+    },
+    MuiBackdrop: {
+      root: {
+        backgroundColor: `${themeConstants.grey.lighter}85`
+      }
+    },
+    MuiInput: {
+      root: themeConstants.grey.main
+    }
+  },
   components: {
     MuiButton: {
       styleOverrides: {
@@ -89,7 +197,7 @@ const rubiwinBaseTheme = {
           color: themeConstants.white.main
         },
         iconSizeLarge: {
-          '&> *:first-of-type': {
+          '&> *:first-child': {
             fontSize: '25px'
           }
         }
@@ -104,16 +212,6 @@ const rubiwinBaseTheme = {
             '&:hover': {
               backgroundColor: themeConstants.primary.main + 'FF'
             }
-          }
-        }
-      }
-    },
-    MuiSelect: {
-      styleOverrides: {
-        select: {
-          fontFamily: themeConstants.typography.fontFamily,
-          '&:focus': {
-            backgroundColor: `${themeConstants.primary.main}0D`
           }
         }
       }
@@ -158,12 +256,7 @@ const rubiwinBaseTheme = {
     MuiCheckbox: {
       styleOverrides: {
         root: {
-          '&.Mui-disabled': {
-            color: themeConstants.grey.dynamite
-          }
-        },
-        colorPrimary: {
-          color: themeConstants.primary.main
+          color: themeConstants.grey.light
         }
       }
     },
@@ -214,86 +307,13 @@ const rubiwinBaseTheme = {
     },
     MuiInput: {
       styleOverrides: {
-        root: {
-          backgroundColor: 'white',
-          borderRadius: themeConstants.borderRadius.sm,
-          border: `1px solid ${themeConstants.grey.dynamite}`,
-          padding: '2px 6px',
-          transition: themeConstants.transition.default,
-          '&:hover': {
-            border: `1px solid ${darken(themeConstants.grey.dynamite, 0.1)}`
-          },
-          '&.Mui-focused': {
-            border: `1px solid ${themeConstants.primary.main}`,
-            borderRadius: themeConstants.borderRadius.sm,
-            transition: themeConstants.transition.default
-          },
-          '&.Mui-disabled': {
-            backgroundColor: themeConstants.grey.disable,
-            cursor: 'not-allowed'
-          },
-          '&.Mui-error': {
-            border: `1px solid ${themeConstants.red.main}`,
-            '&:hover': {
-              border: `1px solid ${darken(themeConstants.red.main, 0.05)}`
-            }
-          },
-          '&.MuiInput-underline': {
-            border: '0'
-          }
-        }
+        root: themeConstants.grey.main
       }
     },
     MuiPaper: {
       styleOverrides: {
         root: {
           boxShadow: 'none'
-        }
-      }
-    },
-    MuiFormControlLabel: {
-      styleOverrides: {
-        label: {
-          fontWeight: 'bold',
-          fontSize: themeConstants.typography.smaller
-        }
-      }
-    },
-    MuiInputLabel: {
-      styleOverrides: {
-        root: {
-          fontWeight: 'bold',
-          fontSize: themeConstants.typography.small,
-          marginLeft: '6px',
-          marginBottom: '6px',
-          zIndex: '2'
-        }
-      }
-    },
-    MuiMenuItem: {
-      styleOverrides: {
-        root: {
-          '&:hover': {
-            backgroundColor: `${themeConstants.primary.main}08`
-          }
-        },
-        selected: {
-          color: 'white',
-          backgroundColor: `${themeConstants.primary.main}14`
-        }
-      }
-    },
-    MuiPickersDay: {
-      styleOverrides: {
-        root: {
-          '&.Mui-selected': {
-            '&:hover': {
-              backgroundColor: themeConstants.primary.main
-            },
-            '&:focus': {
-              backgroundColor: themeConstants.primary.main
-            }
-          }
         }
       }
     }
