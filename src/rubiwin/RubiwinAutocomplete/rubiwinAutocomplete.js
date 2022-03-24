c904184bc4eaac0934f47447eb7477dbb4d2369b
Lucas Hubert <l.hubert@redspher.com>
2022-03-24 08:02:20 +0100

    update autocomplete Rubiwin

--- src/rubiwin/RubiwinAutocomplete/rubiwinAutocomplete.js
+++ src/rubiwin/RubiwinAutocomplete/rubiwinAutocomplete.js
@@ -4,42 +4,57 @@ import RubiwinCaretBottomIcon from '../../icons/Rubiwin/CaretBottomIcon'
 import { Autocomplete, TextField, Typography } from '@mui/material'
 import RubiwinCheckbox from '../RubiwinCheckbox'
 
-const RubiwinAutocomplete = ({ label, options, ...props }) => (
-  <Autocomplete
-    options={options}
-    disableClearable
-    popupIcon={<RubiwinCaretBottomIcon />}
-    renderInput={(params) => (
-      <TextField {...params} label={label} variant='standard' />
-    )}
-    renderOption={(props, option, { selected }) => (
+const RubiwinAutocomplete = ({
+  label,
+  options,
+  multiple = false,
+  ...props
+}) => {
+  const multipleProps = {}
+
+  if (multiple) {
+    multipleProps.renderOption = (props, option, { selected }) => (
       <li {...props}>
         <RubiwinCheckbox sx={{ mr: '10px' }} checked={selected} size='small' />
         <Typography variant='body2'>{option.label}</Typography>
       </li>
-    )}
-    getLimitTagsText={(more) => (
-      <Typography variant='body2'>(+{more})</Typography>
-    )}
-    renderTags={(values, getTagProps) => {
-      return values.map((value, index) => (
-        <Typography
-          key={`${value}-${index}`}
-          {...getTagProps({ index })}
-          variant='body2'
-        >
-          {value.label}
-          {values.length - 1 !== index && ','}
-        </Typography>
-      ))
-    }}
-    {...props}
-  />
-)
+    )
+  }
+
+  return (
+    <Autocomplete
+      multiple={multiple}
+      options={options}
+      disableClearable
+      popupIcon={<RubiwinCaretBottomIcon />}
+      renderInput={(params) => (
+        <TextField {...params} label={label} variant='standard' />
+      )}
+      getLimitTagsText={(more) => (
+        <Typography variant='body2'>(+{more})</Typography>
+      )}
+      renderTags={(values, getTagProps) => {
+        return values.map((value, index) => (
+          <Typography
+            key={`${value}-${index}`}
+            {...getTagProps({ index })}
+            variant='body2'
+          >
+            {value.label}
+            {values.length - 1 !== index && ','}
+          </Typography>
+        ))
+      }}
+      {...multipleProps}
+      {...props}
+    />
+  )
+}
 
 RubiwinAutocomplete.propTypes = {
-  label: PropTypes.string,
-  options: PropTypes.array
+  label: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+  options: PropTypes.array,
+  multiple: PropTypes.bool
 }
 
 export default RubiwinAutocomplete
