a264f1f4770c30b691af2cd2aa34ac4bdf275440
Lucas Hubert <l.hubert@redspher.com>
2022-03-30 09:23:54 +0200

    add scroll to top with pagination

--- src/rubiwin/RubiwinPagination/RubiwinPagination.js
+++ src/rubiwin/RubiwinPagination/RubiwinPagination.js
@@ -7,28 +7,53 @@ import CaretRightIcon from '../../icons/Rubiwin/CaretRightIcon'
 const RubiwinPagination = ({
   count,
   onPageChange,
+  table,
   page = 1,
-  rowsPerPage = 10
-}) => (
-  <Pagination
-    count={Math.ceil(count / rowsPerPage)}
-    defaultPage={page}
-    renderItem={(item) => (
-      <PaginationItem
-        components={{ previous: CaretLeftIcon, next: CaretRightIcon }}
-        {...item}
-      />
-    )}
-    onChange={onPageChange}
-    color='primary'
-  />
-)
+  rowsPerPage = 10,
+  spacingScrollTop = 15
+}) => {
+  const scrollToTop = (event, newPage) => {
+    if (page !== newPage) {
+      if (
+        table &&
+        window.scrollY > table.current.offsetTop - spacingScrollTop
+      ) {
+        window.scrollTo({
+          top: table.current.offsetTop - spacingScrollTop,
+          behavior: 'smooth'
+        })
+      }
+
+      onPageChange(event, newPage)
+    }
+  }
+
+  return (
+    <Pagination
+      count={Math.ceil(count / rowsPerPage)}
+      defaultPage={page}
+      renderItem={(item) => (
+        <PaginationItem
+          components={{ previous: CaretLeftIcon, next: CaretRightIcon }}
+          {...item}
+        />
+      )}
+      onChange={scrollToTop}
+      color='primary'
+    />
+  )
+}
 
 RubiwinPagination.propTypes = {
   count: PropTypes.number.isRequired,
   onPageChange: PropTypes.func.isRequired,
   page: PropTypes.number,
-  rowsPerPage: PropTypes.number
+  rowsPerPage: PropTypes.number,
+  table: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.shape({ current: PropTypes.object })
+  ]),
+  spacingScrollTop: PropTypes.number
 }
 
 export default RubiwinPagination
