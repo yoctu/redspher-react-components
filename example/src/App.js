2467f006230203a069f07f77007546c593ec3f0a
Skopal Philippe <philippe@redspher.com>
2022-09-22 09:03:08 +0200

    Added the icon

--- example/src/App.js
+++ example/src/App.js
@@ -197,7 +197,8 @@ import {
   RubiwinPagination,
   RubiwinDatePicker,
   E4PlaneIcon,
-  StopWatchIcon
+  StopWatchIcon,
+  MessageIcon
 } from 'redspher-components'
 import {
   Button,
@@ -1419,6 +1420,7 @@ const ShipperShowCase = () => {
           <StatisticsReportAvailableIcon title='StatisticsReportAvailableIcon' />
           <E4PlaneIcon title='E4PlaneIcon' />
           <StopWatchIcon title='StopWatchIcon' />
+          <MessageIcon title='MessageIcon' />
         </section>
         <section
           style={{
